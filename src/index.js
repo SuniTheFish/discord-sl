e7414525a81a67c724e1e4ad95ce7d41ff25c888
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-05-18 12:19:17 -0700

    clarity improvments to async code with async/await

--- src/index.js
+++ src/index.js
@@ -11,19 +11,18 @@ client.on('ready', () => {
 
 const VIEW_WIDTH = 50;
 
-client.on('message', (msg) => {
+client.on('message', async (msg) => {
   if (msg.content === 'sl') {
-    let chain = msg.channel.send(
+    let lastMsg = await msg.channel.send(
       `\`\`\`${locomotive.get(1, VIEW_WIDTH)}\`\`\``,
     );
     for (let i = 2; i < locomotive.length + VIEW_WIDTH - 1; i += 1) {
-      // add a delay of 1 second (because rate limiting), then add the next promise after it
-      // I thought about using async/await, but my style guide got a bit grumpy, and it wouldn't
-      // be a massive saving in terms of amount of code (probably would be clearer tbh)
-      chain = chain.then((sentMsg) => new Promise((r) => setTimeout(r, 1000))
-        .then(() => sentMsg.edit(
-          `\`\`\`${locomotive.get(i, VIEW_WIDTH)}\`\`\``,
-        )));
+      // the delay is the whole point (due to rate limiting), so this warning is invalid
+      /* eslint-disable no-await-in-loop */
+      // add a delay of 1 second (because rate limiting)
+      await new Promise((r) => setTimeout(r, 1000));
+      lastMsg = await lastMsg.edit(`\`\`\`${locomotive.get(i, VIEW_WIDTH)}\`\`\``);
+      /* eslint-enable no-await-in-loop */
     }
   }
 });
